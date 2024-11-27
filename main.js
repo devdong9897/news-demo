49b92cfaf8270242c096df3b96310140ff84be6e
devdong9897 <dev.dong9897@gmail.com>
2024-11-27 22:33:24 +0900

    페이지네이션 공부중

--- main.js
+++ main.js
@@ -9,16 +9,24 @@ let url = new URL(
   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 );
 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
 const getNews = async () => {
   try {
     const response = await fetch(url);
     const data = await response.json();
+    console.log("ddd", data);
     if (response.status === 200) {
       if (data.articles.length === 0) {
         throw new Error("NO result for this search");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     } else {
       throw new Error(data.message);
     }
@@ -82,6 +90,74 @@ const errorRender = (errorMessage) => {
 
 getLatestNews();
 
+const paginationRender = () => {
+  // totalResults,
+  // page,
+  // pageSize,
+  // groupSize,
+
+  // pageGroup,
+  const pageGroup = Math.ceil(page / groupSize);
+  // lastPage
+  const lastPage = pageGroup * groupSize;
+  // firstPage
+  const firstPage = lastPage - (groupSize - 1);
+
+  let paginationHTML = ``;
+
+  for (let i = firstPage; i < lastPage; i++) {
+    paginationHTML += `<li class="page-item">
+        <a class="page-link" href="#">
+          ${i}
+        </a>
+      </li>`;
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+  // <nav aria-label="Page navigation example">
+  //   <ul class="pagination">
+  // <li class="page-item">
+  //   <a class="page-link" href="#">
+  //     Previous
+  //   </a>
+  //     </li>
+  // <li class="page-item">
+  //   <a class="page-link" href="#">
+  //     1
+  //   </a>
+  // </li>
+  //     <li class="page-item">
+  //       <a class="page-link" href="#">
+  //         2
+  //       </a>
+  //     </li>
+  //     <li class="page-item">
+  //       <a class="page-link" href="#">
+  //         3
+  //       </a>
+  //     </li>
+  //     <li class="page-item">
+  //       <a class="page-link" href="#">
+  //         Next
+  //       </a>
+  //     </li>
+  //   </ul>
+  // </nav>;
+};
+
 // 1. 버튼들에 클릭이벤트주기
 // 2. 카테고리별 뉴스가져오기
 // 3. 그 뉴스를 보여주기
+
+// 페이지네이션
+// pageSize(한 페이지에 보여줄 데이터 갯수 ) = 20
+// page(현재 어떤 페이지를 보고 있는지) = 3
+// totalResults(전체 데이터 총 갯수) = 101
+
+// 상황예시
+// totalResults = 101
+// pageSize = 10
+// page = 3
+// groupSize(페이지네이션을 5개씩만 보여줄것이다) = 5
+// totalPage = totalResults / pageSize = 10.1개 올림해서 11개
+// pageGroup(내가 속해있는 페이지그룹) = page / groupSize = 속한 그룹
