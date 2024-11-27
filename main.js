576716f393722f665adeceb5811ea16d07287e6a
devdong9897 <dev.dong9897@gmail.com>
2024-11-27 15:06:48 +0900

    render함수 작성

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_KEY = "6c273fcb2a95445a8f44877d43367af8";
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
   const url = new URL(
@@ -7,8 +7,30 @@ const getLatestNews = async () => {
   );
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log(news);
+  newsList = data.articles;
+  render();
+  console.log(newsList);
 };
 
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => ` <div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+              src=${news.urlToImage}
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}다</h2>
+            <p>${news.description}</p>
+            <div>${news.source.name} * ${news.publishedAt}</div>
+          </div>
+        </div>`
+    )
+    .join(" ");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 getLatestNews();
