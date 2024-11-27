94b4c2f25a58648ca5432f35a0638d9c710bb9ff
devdong9897 <dev.dong9897@gmail.com>
2024-11-27 15:50:17 +0900

    category 검색기능

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const API_KEY = "6c273fcb2a95445a8f44877d43367af8";
 let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
 const getLatestNews = async () => {
   const url = new URL(
@@ -10,6 +15,19 @@ const getLatestNews = async () => {
   newsList = data.articles;
   render();
   console.log(newsList);
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  newsList = data.articles;
+  render();
 };
 
 const render = () => {
@@ -34,3 +52,7 @@ const render = () => {
   document.getElementById("news-board").innerHTML = newsHTML;
 };
 getLatestNews();
+
+// 1. 버튼들에 클릭이벤트주기
+// 2. 카테고리별 뉴스가져오기
+// 3. 그 뉴스를 보여주기
