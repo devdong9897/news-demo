1eda846fbed59ca8c36527eb2502ac535c073520
devdong9897 <dev.dong9897@gmail.com>
2024-11-27 13:45:45 +0900

    news api 호출하기

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const API_KEY = "6c273fcb2a95445a8f44877d43367af8";
+let news = [];
 
 const getLatestNews = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   );
   const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log(news);
 };
 
 getLatestNews();
